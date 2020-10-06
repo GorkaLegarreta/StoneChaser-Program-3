6e380d8a0e06ef8c8b12b019c82607d3506a8628
guerr <guerr@10.164.59.104>
2020-10-06 18:43:08 +0200

    Segundo estado del proyecto

--- StoneChaser/src/Main.java
+++ StoneChaser/src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Este es el estado inicial de nuestro proyecto.");
+		System.out.println("Este es el segundo estado del proyecto");
 	}
 
 }
